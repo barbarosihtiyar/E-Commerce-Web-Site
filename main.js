e34f3dda18c719e578f2fe985541438fc0ffd9b3
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-12-27 17:33:24 +0300

    partTransitionJavascript

--- main.js
+++ main.js
@@ -22,3 +22,82 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
 }
+
+
+const cartBorder = document.querySelector(".cartBorder");
+const textAll = document.getElementById("textAll");
+const textSunglasses = document.getElementById("textSunglasses");
+const textTrousers = document.getElementById("textTrousers");
+const textNecklace = document.getElementById("textNecklace");
+const sunglasses = document.getElementsByClassName("sunglasses");
+const trousers = document.getElementsByClassName("trousers");
+const necklace = document.getElementsByClassName("necklace");
+
+textAll.addEventListener("click",function(){
+  textAll.classList.add("cartBorder");
+  textSunglasses.classList.remove("cartBorder");
+  textTrousers.classList.remove("cartBorder");
+  textNecklace.classList.remove("cartBorder");
+  for(let i=0 ; i<sunglasses.length ; i++){
+    sunglasses[i].style.display="block";
+  }
+  for(let i=0 ; i<sunglasses.length ; i++){
+    trousers[i].style.display="block";
+  }
+
+  for(let i=0 ; i<sunglasses.length ; i++){
+    necklace[i].style.display="block";
+  }
+});
+
+textSunglasses.addEventListener("click",function(){
+  textSunglasses.classList.add("cartBorder");
+  textAll.classList.remove("cartBorder");
+  textTrousers.classList.remove("cartBorder");
+  textNecklace.classList.remove("cartBorder");
+  for(let i=0 ; i<sunglasses.length ; i++){
+    sunglasses[i].style.display="block";
+  }
+  for(let i=0 ; i<sunglasses.length ; i++){
+    trousers[i].style.display="none";
+  }
+
+  for(let i=0 ; i<sunglasses.length ; i++){
+    necklace[i].style.display="none";
+  }
+});
+
+textTrousers.addEventListener("click",function(){
+  textTrousers.classList.add("cartBorder");
+  textSunglasses.classList.remove("cartBorder");
+  textAll.classList.remove("cartBorder");
+  textNecklace.classList.remove("cartBorder");
+  for(let i=0 ; i<sunglasses.length ; i++){
+    sunglasses[i].style.display="none";
+  }
+  for(let i=0 ; i<sunglasses.length ; i++){
+    trousers[i].style.display="block";
+  }
+
+  for(let i=0 ; i<sunglasses.length ; i++){
+    necklace[i].style.display="none";
+  }
+});
+
+textNecklace.addEventListener("click",function(){
+  textNecklace.classList.add("cartBorder");
+  textSunglasses.classList.remove("cartBorder");
+  textTrousers.classList.remove("cartBorder");
+  textAll.classList.remove("cartBorder");
+  for(let i=0 ; i<sunglasses.length ; i++){
+    sunglasses[i].style.display="none";
+  }
+  for(let i=0 ; i<sunglasses.length ; i++){
+    trousers[i].style.display="none";
+  }
+
+  for(let i=0 ; i<sunglasses.length ; i++){
+    necklace[i].style.display="block";
+  }
+
+});
